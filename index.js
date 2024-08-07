a43adf5f16a7bc2773e6644855f15fac4c9e4f44
dexthecool <sraallison@yahoo.com>
2024-08-07 18:34:00 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -23,8 +23,51 @@ class SystemInfo {
                     opcode: 'getBatteryPercentage',
                     blockType: Scratch.BlockType.REPORTER,
                     text: 'battery percentage'
+                },
+                {
+                    opcode: 'getAmbientLight',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'ambient light'
+                },
+                {
+                    opcode: 'getGyro',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'gyroscope [AXIS]',
+                    arguments: {
+                        AXIS: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: 'axisMenu'
+                        }
+                    }
+                },
+                {
+                    opcode: 'getAccelerometer',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'accelerometer [AXIS]',
+                    arguments: {
+                        AXIS: {
+                            type: Scratch.ArgumentType.STRING,
+                            menu: 'axisMenu'
+                        }
+                    }
+                },
+                {
+                    opcode: 'getTemperature',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'temperature'
+                },
+                {
+                    opcode: 'getPressure',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'pressure'
                 }
-            ]
+            ],
+            menus: {
+                axisMenu: {
+                    acceptReporters: true,
+                    items: ['x', 'y', 'z']
+                }
+            }
         };
     }
 
@@ -37,13 +80,117 @@ class SystemInfo {
     }
 
     getRefreshRate() {
-        // This is a placeholder as JS doesn't provide a direct way to get refresh rate
-        return 60; // Assuming a common refresh rate
+        return new Promise((resolve) => {
+            let frameCount = 0;
+            const start = performance.now();
+
+            function countFrames(now) {
+                frameCount++;
+                if (now - start < 1000) {
+                    requestAnimationFrame(countFrames);
+                } else {
+                    resolve(Math.round((frameCount / (now - start)) * 1000));
+                }
+            }
+
+            requestAnimationFrame(countFrames);
+        });
     }
 
     getBatteryPercentage() {
-        return navigator.getBattery().then(battery => {
-            return battery.level * 100;
+        return new Promise((resolve, reject) => {
+            navigator.getBattery().then(battery => {
+                resolve(Math.round(battery.level * 100));
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    getAmbientLight() {
+        return new Promise((resolve, reject) => {
+            if ('AmbientLightSensor' in window) {
+                const sensor = new AmbientLightSensor();
+                sensor.addEventListener('reading', () => {
+                    resolve(sensor.illuminance);
+                });
+                sensor.addEventListener('error', reject);
+                sensor.start();
+            } else {
+                reject('AmbientLightSensor not supported');
+            }
+        });
+    }
+
+    getGyro(args) {
+        return new Promise((resolve, reject) => {
+            if ('Gyroscope' in window) {
+                const sensor = new Gyroscope();
+                sensor.addEventListener('reading', () => {
+                    if (args.AXIS === 'x') {
+                        resolve(sensor.x);
+                    } else if (args.AXIS === 'y') {
+                        resolve(sensor.y);
+                    } else if (args.AXIS === 'z') {
+                        resolve(sensor.z);
+                    }
+                });
+                sensor.addEventListener('error', reject);
+                sensor.start();
+            } else {
+                reject('Gyroscope not supported');
+            }
+        });
+    }
+
+    getAccelerometer(args) {
+        return new Promise((resolve, reject) => {
+            if ('Accelerometer' in window) {
+                const sensor = new Accelerometer();
+                sensor.addEventListener('reading', () => {
+                    if (args.AXIS === 'x') {
+                        resolve(sensor.x);
+                    } else if (args.AXIS === 'y') {
+                        resolve(sensor.y);
+                    } else if (args.AXIS === 'z') {
+                        resolve(sensor.z);
+                    }
+                });
+                sensor.addEventListener('error', reject);
+                sensor.start();
+            } else {
+                reject('Accelerometer not supported');
+            }
+        });
+    }
+
+    getTemperature() {
+        return new Promise((resolve, reject) => {
+            if ('TemperatureSensor' in window) {
+                const sensor = new TemperatureSensor();
+                sensor.addEventListener('reading', () => {
+                    resolve(sensor.temperature);
+                });
+                sensor.addEventListener('error', reject);
+                sensor.start();
+            } else {
+                reject('TemperatureSensor not supported');
+            }
+        });
+    }
+
+    getPressure() {
+        return new Promise((resolve, reject) => {
+            if ('PressureSensor' in window) {
+                const sensor = new PressureSensor();
+                sensor.addEventListener('reading', () => {
+                    resolve(sensor.pressure);
+                });
+                sensor.addEventListener('error', reject);
+                sensor.start();
+            } else {
+                reject('PressureSensor not supported');
+            }
         });
     }
 }
